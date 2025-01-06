bc83d9de9d5fc990566ec1e687a7771bd374036c
José Duarte <jduartea@gmail.com>
2025-01-06 13:05:13 -0300

    refactor: update AppDetails struct to make name, version, buildNumber, and identifier optional

--- Sources/AdMoai/Utils/AppDetails.swift
+++ Sources/AdMoai/Utils/AppDetails.swift
@@ -4,19 +4,18 @@ public func getAppDetails() -> AppDetails {
     let bundle = Bundle.main
     return AppDetails(
         name: bundle.object(forInfoDictionaryKey: "CFBundleDisplayName") as? String
-            ?? bundle.object(forInfoDictionaryKey: "CFBundleName") as? String
-            ?? "Unknown",
-        version: bundle.infoDictionary?["CFBundleShortVersionString"] as? String ?? "Unknown",
-        buildNumber: bundle.infoDictionary?["CFBundleVersion"] as? String ?? "Unknown",
-        identifier: bundle.bundleIdentifier ?? "Unknown",
+            ?? bundle.object(forInfoDictionaryKey: "CFBundleName") as? String,
+        version: bundle.infoDictionary?["CFBundleShortVersionString"] as? String,
+        buildNumber: bundle.infoDictionary?["CFBundleVersion"] as? String,
+        identifier: bundle.bundleIdentifier,
         language: Bundle.main.preferredLocalizations.first
     )
 }
 
 public struct AppDetails {
-    public let name: String
-    public let version: String
-    public let buildNumber: String
-    public let identifier: String
+    public let name: String?
+    public let version: String?
+    public let buildNumber: String?
+    public let identifier: String?
     public let language: String?
 }
